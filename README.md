294f8ad4e9a7fd0c6bf279fe7efb439c69023fd2
Dan Dean <dan.dean@simple.com>
2012-08-03 23:20:53 -0700

    New methods and documentation.
    * create
    * on/off
    * find/findAll

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-DOM Additions
+Element Additions
 ================================================================================
 
-**DOM Additions** extends the DOM API, making it easier to use. You may find this offensive... luckily I don't care about your feelings on the matter.
+**Element Additions** extends the DOM Element API, making it easier to use. You may find this offensive... luckily I don't care about your feelings on the matter.
 
 Should I Use This?
 --------------------------------------------------------------------------------
@@ -13,12 +13,12 @@ Installation
 
 Grab a copy of the source, and require it in your app:
 
-    require("./path/to/dom-additions").install();
+    require("./path/to/element-additions").install();
 
 Or install it with NPM:
 
-    $ npm install dom-additions
+    $ npm install element-additions
 
 Then require it in your app: 
 
-    require("dom-additions").install();
+    require("element-additions").install();
--- index.js
+++ index.js
@@ -7,18 +7,20 @@
     });
   }
 
+  function isNode(value) {
+    return value && (value.nodeType == 1 || value.nodeType == 3);
+  }
+
   function isString(value) {
     return Object.prototype.toString.call(value) === '[object String]';
   }
 
   function isUndefined(value) {
-    return void(0) === value
+    return void(0) === value;
   }
 
   var slice = Array.prototype.slice;
-
   var styleSheetElement;
-
   var Extensions = {
 
     hasClass: function(className) {
@@ -84,6 +86,11 @@
       return this;
     },
 
+    /**
+     * Element#hide() -> Element
+     *
+     * Hides the element.
+    **/
     hide: function() {
       if (!styleSheetElement) {
         styleSheetElement = document.createElement('style');
@@ -99,10 +106,167 @@
       return this;
     },
 
+    /**
+     * Element#show() -> Element
+     *
+     * Shows the element. The 
+    **/
     show: function() {
       this.removeAttribute('data-dom-hidden');
       return this;
-    }
+    },
+
+    /**
+     * Element#find(selector) -> Element
+     * - selector (String): CSS Selector
+     *
+     * Alias of Element#querySelector
+    **/
+    find: function(selector) {
+      return this.querySelector(selector);
+    },
+
+    /**
+     * Element#findAll(selector) -> NodeList
+     * - selector (String): CSS Selector
+     *
+     * Alias of Element#querySelectorAll
+    **/
+    findAll: function(selector) {
+      return this.querySelectorAll(selector);
+    },
+
+    /**
+     * Element#on(eventName[, selector], handler) -> Element
+     * - eventName (String): DOM Event Name to listen to.
+     * - selector (String): Optional CSS Selector.
+     * - handler (function|Object): Event handler of object with `handleEvent` method.
+     *
+     * Provides DOM event observing with optional event delegation when `selector`
+     * is provided.
+     *
+     * Unlike `addEventListener` API, the handler function is bound to the
+     * scope of the matched element, so `this` within the handler will refer
+     * to the element.
+     *
+     * NOTE: To prevent memory leaks, always call `Element#off` before removing
+     * and element from the dom.
+    **/
+    on: function(eventName, selector, handler) {
+      var scope = this,
+          wrappedHandler;
+
+      if (!this.__registry__) {
+        // Lazily create the event registry. 
+        Object.defineProperty(this, '__registry__', {
+          value: [],
+          configurable: false, enumerable: false, writable: true
+        });
+      }
+
+      if (typeof selector != 'undefined' && handler) {
+        wrappedHandler = function(event) {
+          var match = null;
+          if (selector) {
+            var current = event.target;
+            while(current !== scope && match == null) {
+              var matches = current.parentNode.findAll(selector);
+              if (slice.call(matches).indexOf(current) > -1) {
+                match = current;
+              } else {
+                current = current.parentNode;
+              }
+            }
+            if (match) (handler.handleEvent || handler).call(match, event);
+          }
+        };
+
+      } else {
+
+        wrappedHandler = function(event) {
+          (selector.handleEvent || selector).call(scope, event);
+        };
+      }
+
+      this.__registry__.push([eventName, selector, handler, wrappedHandler]);
+      this.addEventListener(eventName, wrappedHandler, false);
+      return this;
+    },
+
+    /**
+     * Element#off([eventName], [selector], [handler]) -> Element
+     * - eventName (String): DOM Event Name to listen to.
+     * - selector (String): Optional CSS Selector.
+     * - handler (function|Object): Event handler of object with `handleEvent` method.
+     *
+     * Removes DOM event observes for the element. When called without,
+     * arguments, all event handlers are removed.
+    **/
+    off: function(eventName, selector, handler) {
+      if (!this.__registry__) return this;
+
+      if (typeof selector == 'undefined' && typeof handler == 'undefined') {
+        this.__registry__.forEach(function(group) {
+          this.removeEventListener(group[0], group[3], false);
+          this.__registry__.length = 0;
+        }.bind(this));
+        return this;
+      }
+
+      for (var i=0; i<this.__registry__.length; i++) {
+        var group = this.__registry__[i];
+        if (group[0] == eventName && group[1] == selector && group[2] == handler) {
+          this.removeEventListener(eventName, group[3], false);
+          var rest = this.__registry__.slice(i+1);
+          this.__registry__.length = i;
+          this.__registry__.push.apply(this.__registry__, rest);
+          break;
+        }
+      }
+
+      return this;
+    },
+
+    /**
+     * Element#create(tag, [attributes], [content]) -> Element
+     * - tag (String): Element tag name.
+     * - attributes (Object): Optional hash of Element attributes
+     * - content (String|Node): Optional content to insert into the new element.
+     *
+     * Creates a new Element.
+     *
+     * TODO: Support namespaced tag names and attributes.
+    **/
+    create: function(name, attributes, content) {
+      var element = document.createElement(name);
+      var contentIsNode, contentIsString, typeChecked;
+
+      if (attributes) {
+        if (!content && ((typeChecked = true, contentIsNode = isNode(attributes)) || (contentIsString = isString(attributes)))) {
+          content = attributes;
+          attributes = null;
+        }
+      }
+
+      if (attributes) {
+        for (var key in attributes) {
+          element.setAttribute(key, attributes[key]);
+        }
+      }
+
+      if (content) {
+        if ((typeChecked && contentIsString) || (!typeChecked && isString(content))) {
+          element.innerHTML = content;
+        } else if ((typeChecked && contentIsNode) || (!typeChecked && isNode(content))) {
+          element.appendChild(content);
+        }
+      }
+
+      return element;
+    },
+ 
+    // Extend NodeList
+    // mouseenter/mouseleave
   };
 
   var Module = {
@@ -133,6 +297,11 @@
 
   // Copy methods over to the Module object.
   Object.keys(Extensions).forEach(function(method) {
+    if (method == 'create') {
+      Module[method] = Extensions[method];
+      return;
+    }
+
     Module[method] = function(element, args) {
       return Extensions[method].apply(element, slice.call(arguments, 1));
     }
