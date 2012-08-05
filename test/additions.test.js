1baf10bcf9abfeab8277ddaff74bd0337e905f0e
Dan Dean <dan.dean@simple.com>
2012-08-05 09:10:36 -0700

    Updated unit tests

--- test/additions.test.js
+++ test/additions.test.js
@@ -23,40 +23,47 @@ describe('DOM Additions', function() {
 
   it('should allow setting styles in various argument formats', function() {
     element.setStyle('float', 'left');
-    assert.ok(element.style.float == 'left');
+    assert.equal('left', element.style.float);
 
     element.setStyle('float:right;');
-    assert.ok(element.style.float == 'right');
+    assert.equal('right', element.style.float);
 
     element.setStyle({ float: 'none' });
-    assert.ok(element.style.float == 'none');
+    assert.equal('none', element.style.float);
   });
 
   it('should be able to show and hide an element', function() {
     assert.ok(element.offsetWidth > 0);
     element.hide();
-    assert.ok(element.offsetWidth == 0);
+    assert.equal(0, element.offsetWidth);
     element.show();
     assert.ok(element.offsetWidth > 0);
   });
 
   it('should be able to manipulate an element\'s class list', function() {
-    assert.ok(element.className == '');
+    assert.equal('', element.className, 'Did not start with an empty className');
 
     element.addClass('rad');
-    assert.ok(element.className == 'rad');
-    assert.ok(element.hasClass('rad'));
+    assert.equal('rad', element.className, 'Did not set className to "rad"');
+    assert.ok(element.hasClass('rad'), 'hasClass did not detect the class');
 
     element.removeClass('rad');
-    assert.ok(element.hasClass('rad') == false);
-
-    element.removeClass('rad');
-    assert.ok(element.className == '');
+    assert.equal(false, element.hasClass('rad'), 'hasClass did not remove the class');
 
     element.toggleClass('rad');
-    assert.ok(element.className == 'rad');
+    assert.equal('rad', element.className, 'toggleClass did not add the class');
 
     element.toggleClass('rad');
-    assert.ok(element.className == '');
+    assert.equal('', element.className, 'toggleClass did not remove the class');
+
+    element.addClass('rad cool');
+    element.addClass('cool super');
+    assert.ok(element.hasClass('rad'), 'addClass did not correctly add multiple classes - rad');
+    assert.ok(element.hasClass('cool'), 'addClass did not correctly add multiple classes - cool');
+    assert.ok(element.hasClass('super'), 'addClass did not correctly add multiple classes - super');
+    assert.equal(element.className.match(/cool/g).length, 1, 'addClass did not correctly add multiple classes - cool should be present once only');
+
+    element.removeClass('rad super');
+    assert.equal('cool', element.className, 'removeClass did not correctly add multiple classes');
   });
 });
