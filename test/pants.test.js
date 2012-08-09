5e80f698085dac2c724a6c49587d7351e6aed078
Dan Dean <dan.dean@simple.com>
2012-08-09 13:49:25 -0700

    Unit tests for Event binding.

--- test/pants.test.js
+++ test/pants.test.js
@@ -65,4 +65,62 @@ describe('Element Pants', function() {
     element.removeClass('rad super');
     assert.equal('cool', element.className, 'removeClass did not correctly add multiple classes');
   });
+
+  it('should bind DOM events', function(done) {
+    checkOnBinding();
+
+    function checkOnBinding() {
+      element.on('click', function(e) {
+        assert.equal(element, this);
+        assert.equal('A', element.nodeName);
+        element.off();
+        checkDelegatedOnBinding();
+      });
+      element.click();
+    }
+
+    function checkDelegatedOnBinding() {
+      document.on('click', element.nodeName.toLowerCase(), function(e) {
+        assert.notEqual(document, this);
+        assert.equal(element, this);
+        assert.equal('A', this.nodeName);
+        document.off();
+        checkOffUnbinding();
+      });
+      element.click();
+    }
+
+    function checkOffUnbinding() {
+      assert.equal(0, element.__registry__.length);
+
+      var handler1 = function(){};
+      var handler2 = function(){};
+      element.on('click', handler1);
+      assert.equal(1, element.__registry__.length, 'should have bound non-delegated handler');
+
+      element.on('click', handler1);
+      assert.equal(2, element.__registry__.length, 'should have bound a 2nd non-delegated handler');
+
+      element.off('click', handler1);
+      assert.equal(1, element.__registry__.length, 'should have removed a non-delegated handler');
+
+      element.on('click', 'x', handler2);
+      assert.equal(2, element.__registry__.length, 'should have added a delegated handler');
+
+      element.off('click', handler2);
+      assert.equal(2, element.__registry__.length, 'should not have removed delegated handler without selector');
+
+      element.off('click', 'x', handler2);
+      assert.equal(1, element.__registry__.length, 'should have removed delegated handler with selector');
+
+      element.on('click', handler1);
+      element.on('click', handler1);
+      element.on('click', handler1);
+      element.off();
+      assert.equal(0, element.__registry__.length, 'should have removed all event handlers');
+
+      done();
+    }
+
+  });
 });
